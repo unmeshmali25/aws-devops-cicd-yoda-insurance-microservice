ceb9a336de68915d863c5dc9571e1e176948561b
Unemsh <unmesh.mali@cvshealth.com>
2024-11-28 11:33:37 -0500

    basic springboot app with dockerfile

---
+++ src/test/java/com/yodainsurance/policy_service/PolicyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yodainsurance.policy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
